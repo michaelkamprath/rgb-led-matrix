1464db43b6636bae1231e146c61bb609b3ca78ea
Michael Kamprath <michael@claireware.com>
2017-06-11 00:15:04 -0700

    added license notice

--- drivers/arduino/RGB_LED_Matrix_Lib/SPIConnection.h
+++ drivers/arduino/RGB_LED_Matrix_Lib/SPIConnection.h
@@ -1,5 +1,20 @@
-
-
+//     RGB Matrix Project - SPIConnection
+//     Copyright (C) 2017 Michael Kamprath
+//
+//     This file is part of RGB Matrix Project.
+// 
+//     RGB Matrix Project is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     RGB Matrix Project is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with RGB Matrix Project.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef __SPICONNECTION_H__
 #define __SPICONNECTION_H__
 
